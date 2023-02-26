beca932436ce378f810045e8212d34de4899e0ba
Lyon <lyon.b.lee@gmail.com>
2023-02-26 16:25:29 -0500

    W07D02 - Updating our App, Update DayList component and import it to Application.js, along with DayListItem, to ensure they are both working in actual application environment

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,7 +3,7 @@ import React from "react";
 import DayListItem from "components/DayListItem";
 
 export default function DayList(props) {
-  
+
   const days = props.days.map((day) => {
     return (
       <DayListItem
@@ -11,7 +11,7 @@ export default function DayList(props) {
         name={day.name}
         spots={day.spots}
         selected={day.name === props.day}
-        setDay={day.setDay}
+        setDay={props.setDay}
       />
     )
   })
