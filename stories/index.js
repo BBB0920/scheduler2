995fcc99fd8af4fdfe2e007bcdedd7aee92d7cb9
Lyon <lyon.b.lee@gmail.com>
2023-02-27 14:39:58 -0500

    W07D03 - Refactoring Interviewer Components, Refactor InterviewerList and InterviewerListItem via prop drilling (threading) interviewer.id into the setInterviewer function

--- stories/index.js
+++ stories/index.js
@@ -97,11 +97,11 @@ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayList
     ))
     .add("Clickable", () => (
       <InterviewerListItem
-        id={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        setInterviewer={action("setInterviewer")}
-      />
+        setInterviewer={() => action("setInterviewer")(interviewer.id)}
+        //This is Storybook syntax - An argument can be passed to a function in Storybook by wrapping it in () after the name of the action
+        />
     ));
 
   const interviewers = [
