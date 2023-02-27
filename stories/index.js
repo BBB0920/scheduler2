6a7567ad4780dcbcf9d1d167d71bc73770841e04
Lyon <lyon.b.lee@gmail.com>
2023-02-27 18:31:02 -0500

    W07D03 - More Appointment Views, Create Show, Confirm, Status and Error components of Appointment component

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,11 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error.js";
+import Form from "components/Appointment/Form.js";
 
 //-----------------------------------------------------------------
 // Button Stories
@@ -180,5 +185,31 @@ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayList
     .add("Empty", () => (
       <Empty
         onAdd={action("onAdd")}
+      />
+    ))
+    .add("Show", () => (
+      <Show
+        student="Lydia Miller-Jones"
+        interviewer={interviewer}
+        onEdit={action("onEdit")}
+        onDelete={action("onDelete")}
+      />
+    ))
+    .add("Confirm", () => (
+      <Confirm
+        message="Delete the appointment?"
+        onConfirm={action("onConfirm")}
+        onCancel={action("onCancel")}
+      />
+    ))
+    .add("Stories", () => (
+      <Status
+        message="Deleting"
+      />
+    ))
+    .add("Error", () => (
+      <Error
+        message="Could not delete appointment"
+        onClose={action("onClose")}
       />
     ))
