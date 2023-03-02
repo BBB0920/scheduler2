7ebff1f124a55fae17070e86af0d2355cb91812f
Lyon <lyon.b.lee@gmail.com>
2023-03-01 21:21:35 -0500

    W07D05 - Using our Custom Hook, Implement UseVisualMode hook into Appointment Component and have it display Empty, Form and Show sub-components when interacting with Empty component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,21 +2,51 @@ import React from "react";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
 
 import "./styles.scss";
+import Form from "./Form";
 
 export default function Appointment(props) {
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
   return (
     <article className="appointment">
       <Header
         time={props.time}
       />
-      {props.interview ? 
+      
+      {mode === EMPTY && 
+        <Empty 
+          onAdd={() => {
+            console.log("Clicked onAdd")
+            transition(CREATE)
+          }}
+        />
+      }
+
+      {mode === CREATE &&
+        <Form
+          interviewers={[]}
+          onCancel={() => {
+            transition(EMPTY)
+          }}
+        />
+      }
+
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-        /> : <Empty></Empty>}
+        />
+      )}
     </article>
   )
 }
